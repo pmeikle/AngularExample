8df0d78ac4643af5877145bcd1ea3beb88bdc25a
Pierce Meikle <pierce.meikle@gmail.com>
2018-03-25 16:11:38 -0400

    Lots of angular for the user management page but still having an issue with the ng2bs3modal component

---
+++ Angular2MVC/Controllers/BaseAPIController.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using Newtonsoft.Json;
+using Angular2MVC.DBContext;
+
+namespace Angular2MVC.Controllers
+{
+    public class BaseApiController : ApiController
+    {
+        protected readonly UserDBEntities UserDB = new UserDBEntities();
+        protected HttpResponseMessage ToJson(dynamic obj)
+        {
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            response.Content = new StringContent(JsonConvert.SerializeObject(obj), Encoding.UTF8, "application/json");
+            return response;
+        }
+    }
+}
---
+++ Angular2MVC/app/app.module.ts
@@ -0,0 +1,20 @@
+﻿import { NgModule } from '@angular/core';
+import { APP_BASE_HREF } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { AppComponent } from './app.component';
+import { routing } from './app.routing';
+import { HomeComponent } from './components/home.component';
+import { UserService } from './service/user.service';
+import { UserComponent } from './components/user.component';
+
+
+@NgModule({
+    imports: [BrowserModule, ReactiveFormsModule, HttpModule, routing],
+    declarations: [AppComponent, HomeComponent, UserComponent],
+    providers: [{ provide: APP_BASE_HREF, useValue: '/' }, UserService],
+    bootstrap: [AppComponent]
+})
+
+export class AppModule { }
